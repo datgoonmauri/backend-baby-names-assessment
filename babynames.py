ea44915036796446887d1fe3970a736349717bd6
AlvaradoM <mauriciomj11@gmail.com>
2020-03-09 06:04:08 -0700

    done

--- babynames.py
+++ babynames.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# BabyNames python coding exercise.
-
+#!/usr/bin/python
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -14,14 +10,11 @@ import sys
 import re
 import argparse
 
-
-"""
+"""Baby Names exercise
 Define the extract_names() function below and change main()
 to call it.
-
 For writing regex, it's nice to include a copy of the target
 text for inspiration.
-
 Here's what the html looks like in the baby.html files:
 ...
 <h3 align="center">Popularity in 1990</h3>
@@ -30,77 +23,64 @@ Here's what the html looks like in the baby.html files:
 <tr align="right"><td>2</td><td>Christopher</td><td>Ashley</td>
 <tr align="right"><td>3</td><td>Matthew</td><td>Brittany</td>
 ...
-
 Suggested milestones for incremental development:
- - Extract the year and print it
- - Extract the names and rank numbers and just print them
- - Get the names data into a dict and print it
- - Build the [year, 'name rank', ... ] list and print it
- - Fix main() to use the extract_names list
+ -Extract the year and print it
+ -Extract the names and rank numbers and just print them
+ -Get the names data into a dict and print it
+ -Build the [year, 'name rank', ... ] list and print it
+ -Fix main() to use the extract_names list
 """
-
 
 def extract_names(filename):
     names = []
     with open(filename) as f:
         text = f.read()
-    baby_year = re.search(r'Popularity\sin\s(d\d\d\d)', text)
-    if not baby_year:
-
-        sys.stderr.write('No year could be found!\n')
-        sys.exit(1)
-    year = baby_year.group(1)
-    names.append(year)
-
-    tuples = re.findall(r'<td>(\d+)</td><td>(\w+)</td>\<td>(\w+)</td>', text)
-    names_rank = {}
+        date_match =  re.search(r'Popularity\sin\s(\d\d\d\d)', text)
+        
+        if not date_match:
+            sys.stderr.write("No year was found")
+            sys.exit(1)
+        year = date_match.group(1)
+        names.append(year)
+        
+        tuples = re.findall(r'<td>(\d+)</td><td>(\w+)</td>\<td>(\w+)</td>', text)
+        print(tuples)
+        
+    rank_names = {}
     for ranked in tuples:
         (rank, boyname, girlname) = ranked
-    if boyname not in names_rank:
-            names_rank[boyname] = rank
-    if girlname not in names_rank:
-            names_rank[girlname] = rank
-    sort_names = sorted(names_rank.keys())
-    for name in sort_names:
-        names.append(name + " " + names_rank[name])
+        if boyname not in rank_names:
+            rank_names[boyname] = rank
+        if girlname not in rank_names:
+            rank_names[girlname] = rank
 
+    sorted_names = sorted(rank_names.keys())
+    for name in sorted_names:
+        names.append(name + " " + rank_names[name])
     return names
 
 
-def create_parser():
-    """Create a cmd line parser object with 2 argument definitions"""
-    parser = argparse.ArgumentParser(description="Extracts and alphabetizes baby names from html.")
-    parser.add_argument('--summaryfile', help='creates a summary file', action='store_true')
-    # The nargs option instructs the parser to expect 1 or more filenames.
-    # It will also expand wildcards just like the shell, e.g. 'baby*.html' will work.
-    parser.add_argument('files', help='filename(s) to parse', nargs='+')
-    return parser
-def main(args):
-    # Create a command-line parser object with parsing rules
-    parser = create_parser()
-    # Run the parser to collect command-line arguments into a NAMESPACE called 'ns'
-    ns = parser.parse_args(args)
-
-    if not ns:
-        parser.print_usage()
-        sys.exit(1)
-        
-        file_list = ns.files
-        
-    for filename in file_list:
-        print("Running File""{}".format(filename))
-        names = extract_names(filename)
-    with open(filename) as f:
-        text = f.read()
-        
-    create_summary = ns.summaryfile
-
-    if create_summary:
-        with open(filename + ".summary", "w") as output:
-            output.write(text + '\n')
+def main():
+    
+  args = sys.argv[1:]
+  
+  if not args:
+    print('usage: [--summaryfile] file [file ...]')
+    sys.exit(1)
+  summary = False
+  if args[0] == '--summaryfile':
+    summary = True
+    del args[0]
+  for filename in args:
+    names = extract_names(filename)
+    text = '\n'.join(names)
+    if summary:
+      outf = open(filename + '.summary', 'w')
+      outf.write(text + '\n')
+      outf.close()
     else:
-        print(text)
+      print(text)
 
 
-    if __name__ == '__main__':
-        main (sys.argv[1:])
+if __name__ == '__main__':
+  main()
